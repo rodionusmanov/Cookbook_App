351a81b00b40cffedba3c1593e243aa3cf73bd50
Mdyachkovskiy <mdyachkovskiy@gmail.com>
2023-07-23 18:48:16 +0500

    MD Добавлена утилита для мониторинга подключения к сети

--- app/src/main/java/com/example/cookbook/di/AppModule.kt
+++ app/src/main/java/com/example/cookbook/di/AppModule.kt
@@ -7,8 +7,11 @@ import com.example.cookbook.model.datasource.RetrofitImplementation
 import com.example.cookbook.model.interactor.SearchFragmentInteractor
 import com.example.cookbook.model.repository.IRepositorySearchRequestToRecipeList
 import com.example.cookbook.model.repository.SearchRepositoryImpl
+import com.example.cookbook.model.repository.network.NetworkRepository
+import com.example.cookbook.utils.network.NetworkLiveData
 import com.example.cookbook.viewModel.searchRecipe.SearchRecipeViewModel
 import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -31,4 +34,7 @@ val appModule = module {
     viewModel {
         SearchRecipeViewModel( get() )
     }
+
+    single { NetworkLiveData(androidContext()) }
+    single { NetworkRepository( get() ) }
 }
