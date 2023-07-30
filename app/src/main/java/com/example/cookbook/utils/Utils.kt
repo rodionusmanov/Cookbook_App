555d13a17cdc66f4ac70058f1eace4a7262dc21c
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2023-07-30 23:25:41 +0500

    MD Удалил конвертор, он пока не нужен

--- app/src/main/java/com/example/cookbook/utils/Utils.kt
+++ app/src/main/java/com/example/cookbook/utils/Utils.kt
@@ -1,19 +1,4 @@
 package com.example.cookbook.utils
 
-import com.example.cookbook.domain.Recipe
-import com.example.cookbook.model.DTO.SearchRecipeListDTO
 
-fun convertSearchDTOToRecipeList(searchRecipeListDTO: SearchRecipeListDTO): List<Recipe> {
-    val recipeList = searchRecipeListDTO.searchRecipeList
-    val returnRecipeList: MutableList<Recipe> = mutableListOf()
-    recipeList.forEach {
-        returnRecipeList.add(
-            Recipe(
-                id = it.id,
-                recipeName = it.title,
-                recipeImage = it.image
-            )
-        )
-    }
-    return returnRecipeList
-}
+
