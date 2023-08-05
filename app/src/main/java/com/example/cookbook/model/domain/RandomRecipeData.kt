7fbc3825b581ce7bc71aaafe23cac628ee0df0aa
Mdyachkovskiy <mdyachkovskiy@gmail.com>
2023-08-05 23:51:42 +0500

    MD - создан класс RandomRecipesListFragment

--- app/src/main/java/com/example/cookbook/model/domain/RandomRecipeData.kt
+++ app/src/main/java/com/example/cookbook/model/domain/RandomRecipeData.kt
@@ -1,8 +1,12 @@
 package com.example.cookbook.model.domain
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class RandomRecipeData(
     val id: Int = 0,
     val image: String = "",
     val summary: String = "",
     val title: String = ""
-)
+) : Parcelable
---
+++ app/src/main/java/com/example/cookbook/view/randomRecipe/RandomRecipesListFragment.kt
@@ -0,0 +1,57 @@
+package com.example.cookbook.view.randomRecipe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.cookbook.databinding.FragmentRandomRecipeListsBinding
+import com.example.cookbook.model.AppState
+import com.example.cookbook.model.domain.RandomRecipeData
+import com.example.cookbook.view.base.BaseFragment
+
+class RandomRecipesListFragment : BaseFragment<AppState>() {
+
+    private var _binding: FragmentRandomRecipeListsBinding? = null
+    private val binding: FragmentRandomRecipeListsBinding get() = _binding!!
+
+    companion object {
+        private const val RANDOM_RECIPE_LISTS_KEY = "RandomRecipesListsKey"
+
+        fun newInstance(randomData: List<RandomRecipeData>): RandomRecipesListFragment {
+            return RandomRecipesListFragment().apply {
+                arguments = Bundle().apply{
+                    putParcelableArrayList(RANDOM_RECIPE_LISTS_KEY, ArrayList(randomData))
+                }
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRandomRecipeListsBinding.inflate(inflater, container, false)
+
+        arguments?.getParcelableArrayList<RandomRecipeData>(RANDOM_RECIPE_LISTS_KEY)?.let{randomData ->
+            setupData(randomData)
+        }
+
+        return binding.root
+    }
+
+    override fun setupData(data: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showErrorDialog(message: String?) {
+        Toast.makeText(context, "Ошибка {$message}", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
