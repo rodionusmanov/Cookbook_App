e742facc4720a57b3ea4bd79c2030dc4c89e0b23
dk <kdi_2004@mail.ru>
2023-08-07 23:46:28 +0300

    добавил тэги

--- app/src/main/java/com/example/cookbook/view/recipeInfo/preparation/PreparationFragment.kt
+++ app/src/main/java/com/example/cookbook/view/recipeInfo/preparation/PreparationFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cookbook.databinding.FragmentPreparationBinding
@@ -63,6 +64,8 @@ class PreparationFragment : Fragment() {
         ingredientAdapter.setData(ingredientsList.distinct())
 
         with(binding) {
+            tvEquipments.isVisible = equipmentList.isNotEmpty()
+            tvIngredient.isVisible = ingredientsList.isNotEmpty()
             rvIngredientsPreparation.layoutManager = ingredientHorizontalLayoutManager
             rvEquipments.layoutManager = equipmentHorizontalLayoutManager
 
