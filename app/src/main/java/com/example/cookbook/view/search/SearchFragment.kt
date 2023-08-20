c45c90025c11366250f3a574796abdbe36f22b2b
dk <kdi_2004@mail.ru>
2023-08-21 00:51:57 +0300

    убрал фокус с поиска, и добавил скрытие клавиатуры

--- app/src/main/java/com/example/cookbook/view/search/SearchFragment.kt
+++ app/src/main/java/com/example/cookbook/view/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Lifecycle
@@ -113,6 +114,8 @@ class SearchFragment : BaseFragment<AppState, List<BaseRecipeData>, FragmentSear
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     query?.let {
                         model.searchRecipeRequest(it, "")
+                        hideKeyboard(binding.searchView)
+
                     }
                     return true
                 }
@@ -151,5 +154,10 @@ class SearchFragment : BaseFragment<AppState, List<BaseRecipeData>, FragmentSear
 
     override fun showErrorDialog(message: String?) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun hideKeyboard(view: View) {
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
