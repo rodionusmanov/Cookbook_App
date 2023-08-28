b01c69f3d61c1ca345516ee93111ac2745dd530f
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2023-08-29 00:04:22 +0500

    MD - удалил 3 кнопку в HomeFragment

--- app/src/main/java/com/example/cookbook/utils/Consts.kt
+++ app/src/main/java/com/example/cookbook/utils/Consts.kt
@@ -3,12 +3,10 @@ package com.example.cookbook.utils
 const val COMPLEX_SEARCH_RECIPE_API = "https://api.spoonacular.com/"
 //const val SPOONACULAR_API_KEY = "e0f820d255c2495698a1eb65d4f195ff"
 
-//const val SPOONACULAR_API_KEY = "5cd80f128c6a4bc583f45c3b53b010be"
+const val SPOONACULAR_API_KEY = "5cd80f128c6a4bc583f45c3b53b010be"
 //const val SPOONACULAR_API_KEY = "e51af98a8a0649d98081a6445393c430"
-const val SPOONACULAR_API_KEY = "c279e4340d6449d6820e2bc4b3ad37cd"
+//const val SPOONACULAR_API_KEY = "c279e4340d6449d6820e2bc4b3ad37cd"
 const val ID = "id"
-const val INGREDIENTS = "ingredients"
-const val INSTRUCTIONS = "instructions"
 
 const val FRAGMENT_HOME = "home"
 const val FRAGMENT_SEARCH = "search_recipe"
