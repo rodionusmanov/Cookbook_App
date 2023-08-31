bc06546a48423b999e8749bae41408258aaf7a15
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2023-08-31 13:46:55 +0500

    MD - удалены ненужные explicit выражения из Koin по рекомендации AS

--- app/src/main/java/com/example/cookbook/model/interactor/HomeFragmentInteractor.kt
+++ app/src/main/java/com/example/cookbook/model/interactor/HomeFragmentInteractor.kt
@@ -1,13 +1,10 @@
 package com.example.cookbook.model.interactor
 
 import com.example.cookbook.model.AppState
-import com.example.cookbook.model.repository.local.LocalRepositoryInfoImpl
 import com.example.cookbook.model.repository.remote.IRepositorySearchRequest
 
 class HomeFragmentInteractor(
-    private val remoteRepository: IRepositorySearchRequest,
-    private val localRepository: LocalRepositoryInfoImpl
-) {
+    private val remoteRepository: IRepositorySearchRequest) {
 
     suspend fun getJokeText(): AppState {
         return AppState.Success(remoteRepository.getJokeText())
